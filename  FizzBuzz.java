57bf449b53ad9f86e4207a2d22ac0eaf39b52b46
hiroki-5 <hite3150@gmail.com>
2022-06-14 16:04:28 +0900

    else ifで処理

---  FizzBuzz.java
+++  FizzBuzz.java
@@ -1,50 +1,43 @@
-public class  FizzBuzz {
+public class FizzBuzz {
 
-    /*コマンドライン引数を受け取る*/
-     public static void main(String[] args) {
-        /*1.で受け取った引数を数値に変換する*/
+	/* コマンドライン引数を受け取る */
+	public static void main(String[] args) {
+		/* 1.で受け取った引数を数値に変換する */
 		if (args.length == 0) {
-			/*2.で変換した数値の回数分、繰り返し処理を行う*/
-            return;
+			/* 2.で変換した数値の回数分、繰り返し処理を行う */
+			return;
 		}
-       /* 1. 表示用の文字列を保持する変数 `result` を定義する*/
+		/* 1. 表示用の文字列を保持する変数 `result` を定義する */
 		int max = Integer.parseInt(args[0]);
 		String result = null;
-    
-        /*3.の繰り返し処理の中で、「何回目の処理か？」を表示する*/
+
+		/* 3.の繰り返し処理の中で、「何回目の処理か？」を表示する */
 		for (int i = 0; i < max; i++) {
 			int num = i + 1;
 			result = "";
-            /*2. 表示しようとしている数字が3で割り切れるか？を確認する
-            - 割り切れるときだけ、変数 `result` に、文字列 `Fizz` を追加する*/
+			/*
+			 * 2. 表示しようとしている数字が3で割り切れるか？を確認する
+			 * - 割り切れるときだけ、変数 `result` に、文字列 `Fizz` を追加する
+			 */
 			if (num % 3 == 0) {
-				result = result + "Fizz";
+				result += "Fizz";
 			}
-           /* 3. 表示しようとしている数字が5で割り切れるか？を確認する
-     - 割り切れるときだけ、変数 `result` に、文字列 `Buzz` を追加する*/
-			if (num % 5 == 0) {
-				result = result + "Buzz";
+			/*
+			 * 3. 表示しようとしている数字が5で割り切れるか？を確認する
+			 * - 割り切れるときだけ、変数 `result` に、文字列 `Buzz` を追加する
+			 */
+			else if (num % 5 == 0) {
+				result += "Buzz";
 			}
-            /*4. 変数 `result` の文字数が `空文字※` かどうか？を確認する
-            - 空文字だったときだけ、変数 `result` に数字を文字列として加える*/
-			if (result.length() == 0) {
-				result = result + num;
+			/*
+			 * 4. 変数 `result` の文字数が `空文字※` かどうか？を確認する
+			 * - 空文字だったときだけ、変数 `result` に数字を文字列として加える
+			 */
+			else {
+				result += num;
 			}
-            /*5. 変数 `result` を表示する*/
+			/* 5. 変数 `result` を表示する */
 			System.out.print(result + " ");
 		}
 	}
 }
-
-
-
- 
- 
- 
-
- 
-
- 
-
-
- 
